9385c8b9aa58f5c71f2ed60305edd41be8ea14a0
diegok <73537261+die4ka@users.noreply.github.com>
2021-04-07 07:44:34 +0100

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@ import Head from 'next/head'
 
 const Home = () => (
   <div>
-    <h1>Next.js on the [JAMstack](https://jamstack.org)</h1>
+    <h1>Hello Next.js</h1>
 
-    <h3>Hooray 🎉 - you've built this with <a href="https://nextjs.org">Next.js</a>!</h3>
+    <h3>Hooray <a href="https://asdata.xyz">asdata.xyz</a>!</h3>
 
     <style jsx>{`
       :global(html,body) {
